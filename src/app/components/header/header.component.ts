3b3f6d5802f76c873f0bae9bd88e4129967968f9
Muhammad Shaheer Uddin <61250463+mshaheeruddin@users.noreply.github.com>
2023-11-15 19:28:47 +0500

    1:52:09

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,14 +1,24 @@
 import { Component } from '@angular/core';
+import { UiService } from 'src/app/services/ui.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.css'],
 })
 export class HeaderComponent {
-    title : string = '+_+'
+  title: string = 'Dark Tasks';
+  showAddTask: boolean;
+  subscription: Subscription;
 
-    toggleAddTask() {
-      console.log('toggle')
-    }
+  constructor(private uiService: UiService) {
+    this.subscription = this.uiService
+      .onToggle()
+      .subscribe((value) => (this.showAddTask = value));
+  }
+
+  toggleAddTask() {
+    this.uiService.toggleAddTask();
+  }
 }
